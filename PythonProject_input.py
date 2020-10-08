e85177258d70d7473cbdf27b0a38dd7159afa4ca
revrard8 <72555899+revrard8@users.noreply.github.com>
2020-10-08 08:30:35 -0400

    Python_Project_input

---
+++ PythonProject_input.py
@@ -0,0 +1,40 @@
+
+'''
+===============================================================================
+ENGR 133 Fa 2020
+
+Assignment Information
+	Assignment:     Py4 Task 7
+	Author:         Rachel Evrard, revrard@purdue.edu
+	Team ID:        LC1-15
+	
+Contributors:  Maria Hubert, hubert3@purdue.edu
+	My contributor(s) helped me:	
+	[x] understand the assignment expectations without
+		telling me how they will approach it.
+	[x] understand different ways to think about a solution
+		without helping me plan my solution.
+	[x] think through the meaning of a specific error or
+		bug present in my code without looking at my code.
+	Note that if you helped somebody else with their code, you
+	have to list that person as a contributor here as well.
+===============================================================================
+'''
+
+import matplotlib.pyplot as plt
+image = plt.imread('Purdue_Arch.png')
+import matplotlib.pyplot as plt
+#Read and Process image file
+plt.imshow(image)
+
+
+'''
+===============================================================================
+ACADEMIC INTEGRITY STATEMENT
+    I have not used source code obtained from any other unauthorized
+    source, either modified or unmodified. Neither have I provided
+    access to my code to another. The project I am submitting
+    is my own original work.
+===============================================================================
+'''
+
