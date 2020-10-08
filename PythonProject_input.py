22f096ca10f72decc624aa55fc1cd7c753f32f87
codery12321 <72549896+codery12321@users.noreply.github.com>
2020-10-08 20:57:26 +0800

    Team project gray scale

--- PythonProject_input.py
+++ PythonProject_input.py
@@ -1,36 +1,49 @@
-
-'''
+"""
 ===============================================================================
 ENGR 133 Fa 2020
 
 Assignment Information
-	Assignment:     Python Project
-	Author:         Team 15
-	Team ID:        LC1-15
+	Assignment:     Py Team Project
+	Author:         Yolanda, chen3633@purdue.edu
+	Team ID:        LC1-15 
 	
-Contributors:   Nathan Budiman, jbudiman@purdue.edu 
-                Collin Gernhardt, cgernhar@purdue.edu
-                Yolanda Chen, chen3633@purdue.edu
+Contributors:   Collin Gernhardt, cgernhar@purdue.edu
                 Rachel Evrard, revrard@purdue.edu
+                Jonathan Budiman, jbudiman@purdue.edu
+               
 	My contributor(s) helped me:	
-	[x] understand the assignment expectations without
+	[ ] understand the assignment expectations without
 		telling me how they will approach it.
-	[x] understand different ways to think about a solution
+	[ ] understand different ways to think about a solution
 		without helping me plan my solution.
-	[x] think through the meaning of a specific error or
+	[ ] think through the meaning of a specific error or
 		bug present in my code without looking at my code.
 	Note that if you helped somebody else with their code, you
 	have to list that person as a contributor here as well.
 ===============================================================================
-'''
 
+"""
+import numpy as np
 import matplotlib.pyplot as plt
 image = plt.imread('Purdue_Arch.png')
-import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 #Read and Process image file
 plt.imshow(image)
 
 
+
+
+def rgbG(RGB):
+    return np.dot(RGB[...,:3], [0.2126, 0.7152, 0.0722])
+
+img = mpimg.imread('Purdue_Arch.png')     
+gray = rgbG(img)    
+plt.imshow(gray, cmap=plt.get_cmap('gray'), vmin=0, vmax=1)
+plt.show()
+
+    
+    
+    
 '''
 ===============================================================================
 ACADEMIC INTEGRITY STATEMENT
